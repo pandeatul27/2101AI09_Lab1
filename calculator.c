ce11afcd4e3eb2471f1a5370aacda407bda0aae4
Atul <pandeatul2702@gmail.com>
2022-07-27 22:54:16 +0530

    Added multiplication

--- calculator.c
+++ calculator.c
@@ -9,6 +9,15 @@ int add()
     return a + b;
 }
 
+int multiply()
+{
+    printf("Enter two numbers :");
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    return a * b;
+}
+
 int subtract()
 {
     printf("Enter first number :");
@@ -20,13 +29,16 @@ int subtract()
 }
 int main()
 {
-    printf("Enter\n1 for addition \n2 for subtraction\n");
+    printf("Enter\n1 for addition \n2 for subtraction3 for multiplication\n");
     int x;
     scanf("%d", &x);
     switch (x)
     {
     case 1:
         printf("Result : %d", add());
+        break;
+    case 3:
+        printf("Result : %d", multiply());
         break;
     default:
         printf("Result : %d", subtract());

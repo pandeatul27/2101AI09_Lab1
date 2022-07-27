3afb867bbc95cd1c0e81f06932184b9d9acf8c18
Atul <pandeatul2702@gmail.com>
2022-07-27 22:29:46 +0530

    Added Merge Sort

--- sorting.c
+++ sorting.c
@@ -54,6 +54,57 @@ void bubble(int a[], int n)
     }
 }
 
+// Merge Sort
+void merge(int a[], int start, int mid, int end)
+{
+    int i, j, k;
+    int n1 = mid - start + 1;
+    int n2 = end - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = a[start + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = a[mid + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = start;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            a[k++] = L[i++];
+        }
+        else
+        {
+            a[k++] = R[j++];
+        }
+    }
+    while (i < n1)
+    {
+        a[k++] = L[i++];
+    }
+
+    while (j < n2)
+        start
+        {
+            a[k++] = R[j++];
+        }
+}
+
+void mergeSort(int a[], int start, int end)
+{
+    if (start < end)
+    {
+        int mid = (start + end) / 2;
+        mergeSort(a, start, mid);
+        mergeSort(a, mid + 1, end);
+        merge(a, start, mid, end);
+    }
+}
 int main()
 {
     int n;
@@ -84,5 +135,12 @@ int main()
         printf("%d ", a[i]);
     }
 
+    mergeSort(a, 0, n - 1);
+    printf("Sorted Array :\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
     return 0;
 }
